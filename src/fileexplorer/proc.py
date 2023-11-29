70cf0fdd94ecfd92644b62880b192b65d14fb480
Ryan Hunter <ryanhunter237@gmail.com>
2023-11-29 08:45:09 +0200

    Docstrings for proc.py

--- src/fileexplorer/proc.py
+++ src/fileexplorer/proc.py
@@ -9,6 +9,15 @@ class ProcessorTemplate(ABC):
     extensions = ()
 
     def can_process_file(self, file_path: Path) -> bool:
+        """
+        Determine if the processor can handle the given file based on its extension.
+
+        Parameters:
+        file_path (Path): The path of the file to check.
+
+        Returns:
+        bool: True if the file can be processed by this processor, False otherwise.
+        """
         return file_path.suffix.lower() in self.extensions
 
     def write_thumbnail(
@@ -17,6 +26,17 @@ class ProcessorTemplate(ABC):
         thumbnails_dir: Path,
         thumbnail_size: tuple[int, int],
     ) -> str:
+        """
+        Generate and save a thumbnail for the provided image.
+
+        Parameters:
+        image (Image.Image): The image object to create a thumbnail from.
+        thumbnails_dir (Path): The directory where the thumbnail should be saved.
+        thumbnail_size (tuple[int, int]): The size of the thumbnail (width, height).
+
+        Returns:
+        str: The filename of the saved thumbnail.
+        """
         image.thumbnail(thumbnail_size)
         image_bytes_io = io.BytesIO()
         image.save(image_bytes_io, format="png")
@@ -34,13 +54,48 @@ class ProcessorTemplate(ABC):
         thumbnails_dir: Path,
         thumbnail_size: tuple[int, int]
     ) -> str | None:
+        """
+        Abstract method to generate a thumbnail from a given file.
+
+        This method should be implemented in subclasses to handle specific file types.
+
+        Parameters:
+        file_path (Path): The path of the file to create a thumbnail from.
+        thumbnails_dir (Path): The directory to save the thumbnail.
+        thumbnail_size (tuple[int, int]): Desired dimensions for the thumbnail.
+
+        Returns:
+        str | None: The filename of the thumbnail if created, otherwise None.
+        """
         pass
 
     @abstractmethod
     def make_data_file(self, file_path: Path, data_files_dir: Path) -> str:
+        """
+        Abstract method to create a data file from the given file.
+
+        This method should be implemented in subclasses to handle specific file types.
+
+        Parameters:
+        file_path (Path): The path of the file to create a data file from.
+        data_files_dir (Path): The directory to save the data file.
+
+        Returns:
+        str: The filename of the created data file.
+        """
         pass
 
     def make_symlink_data_file(self, file_path: Path, data_files_dir: Path) -> str:
+        """
+        Create a symbolic link for the given file in the specified directory.
+
+        Parameters:
+        file_path (Path): The path of the file to create a symbolic link for.
+        data_files_dir (Path): The directory where the symbolic link will be created.
+
+        Returns:
+        str: The filename of the symbolic link.
+        """
         extension = file_path.suffix
         with open(file_path, "rb") as f:
             file_bytes = f.read()
