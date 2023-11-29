c6138087253f81365e0a3b3c40764311171b0f02
Ryan Hunter <ryanhunter237@gmail.com>
2023-11-29 07:23:28 +0200

    Add tests for image_proc

---
+++ tests/test_image_proc.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+from unittest.mock import patch, MagicMock
+import pytest
+from PIL import Image
+
+from fileexplorer.image_proc import ImageProcessor
+
+@pytest.fixture
+def image_processor():
+    return ImageProcessor()
+
+@patch('PIL.Image.open', return_value=MagicMock(spec=Image.Image))
+def test_make_thumbnail_success(
+    mocked_open: MagicMock,
+    tmp_path: Path,
+    image_processor: ImageProcessor
+):
+    thumbnail_path = tmp_path / "thumbnails"
+    thumbnail_path.mkdir()
+    file_path = tmp_path / "image.jpg"
+    thumbnail_filename = image_processor.make_thumbnail(
+        file_path, thumbnail_path, (100, 100)
+    )
+    assert thumbnail_filename.endswith('.png')
+    mocked_open.assert_called_once_with(file_path)
+
+@patch('PIL.Image.open', side_effect=Exception)
+def test_make_thumbnail_fail(
+    mocked_open: MagicMock,
+    tmp_path: Path,
+    image_processor: ImageProcessor
+):
+    thumbnail_path = tmp_path / "thumbnails"
+    thumbnail_path.mkdir()
+    file_path = tmp_path / "image.jpg"
+    thumbnail_filename = image_processor.make_thumbnail(
+        file_path, thumbnail_path, (100, 100)
+    )
+    assert thumbnail_filename is None
+    mocked_open.assert_called_once_with(file_path)
+
+@patch('fileexplorer.proc.ProcessorTemplate.make_symlink_data_file')
+def test_make_data_file(
+    mock_symlink: MagicMock,
+    tmp_path: Path,
+    image_processor: ImageProcessor
+):
+    data_files_dir = tmp_path / "data"
+    data_files_dir.mkdir()
+    file_path = tmp_path / "image.jpg"
+    file_path.touch()
+    image_processor.make_data_file(file_path, data_files_dir)
+    mock_symlink.assert_called_once_with(file_path=file_path, data_files_dir=data_files_dir)
