6597d24652be3cee49e384fbad9bd160671d3454
Ryan Hunter <ryanhunter237@gmail.com>
2023-11-30 07:36:54 +0200

    More tests for directory-parts api

--- tests/test_api.py
+++ tests/test_api.py
@@ -22,14 +22,15 @@ def create_test_app(root_dir: Path, instance_dir: Path) -> Flask:
 @pytest.fixture(scope="session")
 def nested_directories_root_dir(tmp_path_factory: TempPathFactory) -> Path:
     root_dir = tmp_path_factory.mktemp('root-dir')
-    (root_dir / 'subdir1/subdir2/subdir3').mkdir(parents=True)
+    (root_dir / 'subdir1/subdir2').mkdir(parents=True)
+    (root_dir / 'file.txt').touch()
     return root_dir
 
 @pytest.fixture(scope="session")
 def nested_directories_client(
     nested_directories_root_dir: Path,
     tmp_path_factory: TempPathFactory
-) -> tuple[Path, FlaskClient]:
+) -> FlaskClient:
     instance_dir = tmp_path_factory.mktemp('instance-dir')
     app = create_test_app(nested_directories_root_dir, instance_dir)
     return app.test_client()
@@ -49,6 +50,56 @@ def test_directory_parts_redirect(nested_directories_client: FlaskClient):
     assert response.status_code == 308
     redirect_location = response.headers['Location']
     assert urlparse(redirect_location).path == '/api/directory-parts/'
+
+def test_directory_parts_subdir(
+    nested_directories_root_dir: Path,
+    nested_directories_client: FlaskClient
+):
+    response = nested_directories_client.get('/api/directory-parts/subdir1')
+    assert response.status_code == 200
+    parts = response.json
+    assert parts[0]['part'] == nested_directories_root_dir.as_posix()
+    assert urlparse(parts[0]['directory-info-url']).path == '/api/directory-info/'
+    assert parts[1]['part'] == 'subdir1'
+    assert urlparse(parts[1]['directory-info-url']).path == '/api/directory-info/subdir1'
+
+def test_directory_parts_subdir_trailing_slash(
+    nested_directories_root_dir: Path,
+    nested_directories_client: FlaskClient
+):
+    response = nested_directories_client.get('/api/directory-parts/subdir1/')
+    assert response.status_code == 200
+    parts = response.json
+    assert parts[0]['part'] == nested_directories_root_dir.as_posix()
+    assert urlparse(parts[0]['directory-info-url']).path == '/api/directory-info/'
+    assert parts[1]['part'] == 'subdir1'
+    assert urlparse(parts[1]['directory-info-url']).path == '/api/directory-info/subdir1'
+
+def test_directory_parts_multiple_subdirs(
+    nested_directories_root_dir: Path,
+    nested_directories_client: FlaskClient
+):
+    response = nested_directories_client.get('/api/directory-parts/subdir1/subdir2')
+    assert response.status_code == 200
+    parts = response.json
+    assert parts[0]['part'] == nested_directories_root_dir.as_posix()
+    assert urlparse(parts[0]['directory-info-url']).path == '/api/directory-info/'
+    assert parts[1]['part'] == 'subdir1'
+    assert urlparse(parts[1]['directory-info-url']).path == '/api/directory-info/subdir1'
+    assert parts[2]['part'] == 'subdir2'
+    assert urlparse(parts[2]['directory-info-url']).path == '/api/directory-info/subdir1/subdir2'
+
+def test_directory_parts_missing_subdir(nested_directories_client: FlaskClient):
+    response = nested_directories_client.get('/api/directory-parts/missing-subdir')
+    assert response.status_code == 404
+
+def test_directory_parts_on_file(
+    nested_directories_root_dir: Path,
+    nested_directories_client: FlaskClient
+):
+    assert (nested_directories_root_dir / 'file.txt').exists()
+    response = nested_directories_client.get('/api/directory-parts/file.txt')
+    assert response.status_code == 404
 
 
 # @pytest.fixture(scope="session")
