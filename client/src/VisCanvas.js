ef4162ff75617d18c9b32991a83d5b05633b8c49
Ryan Hunter <ryanhunter237@gmail.com>
2023-12-12 07:46:03 +0200

    Add PdfCanvas

--- client/src/VisCanvas.js
+++ client/src/VisCanvas.js
@@ -1,13 +1,36 @@
+import "./VisCanvas.css";
+
+const ImgCanvas = ({ dataUrl }) => {
+  return <img src={dataUrl} className="img-fluid" />;
+};
+
+const PdfCanvas = ({ dataUrl }) => {
+  return (
+    <embed
+      src={`${dataUrl}#toolbar=0&navpanes=0&scrollbar=0`}
+      type="application/pdf"
+      className="pdf-canvas"
+    />
+  );
+};
+
 const VisCanvas = ({ canvasInfo }) => {
   // 'image', 'pdf', 'stl'
   if (canvasInfo.fileType === "image") {
     return (
       <div id="vis-panel">
-        <img src={canvasInfo.dataUrl} className="img-fluid" />
+        <ImgCanvas dataUrl={canvasInfo.dataUrl} />
       </div>
     );
+  } else if (canvasInfo.fileType === "pdf") {
+    return (
+      <div id="vis-panel">
+        <PdfCanvas dataUrl={canvasInfo.dataUrl} />
+      </div>
+    );
+  } else {
+    return <div id="vis-panel">{canvasInfo.dataUrl}</div>;
   }
-  return <div id="vis-panel">{canvasInfo.dataUrl}</div>;
 };
 
 export default VisCanvas;
