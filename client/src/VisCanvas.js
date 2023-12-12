e4e3c2bcee839f60fee46b4938a8a3b460fd2ace
Ryan Hunter <ryanhunter237@gmail.com>
2023-12-12 07:05:53 +0200

    Display images in VisCanvas

--- client/src/VisCanvas.js
+++ client/src/VisCanvas.js
@@ -1,5 +1,13 @@
 const VisCanvas = ({ canvasInfo }) => {
-  return <div>{canvasInfo.dataUrl}</div>;
+  // 'image', 'pdf', 'stl'
+  if (canvasInfo.fileType === "image") {
+    return (
+      <div id="vis-panel">
+        <img src={canvasInfo.dataUrl} className="img-fluid" />
+      </div>
+    );
+  }
+  return <div id="vis-panel">{canvasInfo.dataUrl}</div>;
 };
 
 export default VisCanvas;
